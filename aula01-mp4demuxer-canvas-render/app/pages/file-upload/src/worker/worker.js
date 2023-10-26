caa2100ccb2d8bcfe5aca1c422221b98f5c94e96
Bryan Gomes <bryandossantosg8@gmail.com>
2023-10-26 15:36:25 -0300

    finalizado aula 1 implementado mp4box para processamento de video e worker para o processamento em paralelo

---
+++ aula01-mp4demuxer-canvas-render/app/pages/file-upload/src/worker/worker.js
@@ -0,0 +1,49 @@
+import VideoProcessor from "./videoProcessor.js"
+import MP4Demuxer from "./MP4Demuxer.js";
+import CanvasRenderer from "./canvasRenderer.js"
+
+const qvgaContraints = {
+    width: 320,
+    height: 240
+}
+
+const vgaContraints = {
+    width: 640,
+    height: 480
+}
+
+const hdContraints = {
+    width: 1280,
+    height: 720
+}
+
+const encoderConfig = {
+    ...qvgaContraints,
+    bitrate: 10e6,
+    // WebM
+    codec: 'vp09.00.10.08',
+    pt: 4,
+    hardwareAcceleration: 'prefer-software'
+    // Mp4
+    // codec: 'avc1.42002A',
+    // pt: 1,
+    // hardwareAcceleration: 'prefer-hardware',
+    // avc: { format: 'annexb' }
+}
+
+const mp4Demuxer = new MP4Demuxer()
+const videoProcessor = new VideoProcessor({ mp4Demuxer })
+
+onmessage = async ({ data }) => {
+
+    const renderFrame = CanvasRenderer.getRenderer(data.canvas)
+
+    await videoProcessor.start({
+        file: data.file,
+        renderFrame,
+        encoderConfig,
+        sendMessage(message) {
+            self.postMessage(message)
+        }
+    })
+}
